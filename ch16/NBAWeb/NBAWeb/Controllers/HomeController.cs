3a2794b19039690dcc7a4ad2bc40597c317e91ee
Willy Fang <w70024@gmail.com>
2021-07-09 21:54:14 +0800

    Learning C# ASP.NET MVC part1

---
+++ ch16/NBAWeb/NBAWeb/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NBAWeb.Models;
+
+namespace NBAWeb.Controllers
+{
+    public class HomeController : Controller // HomeController控制器繼承自Controller
+    {
+        NBADBEntities db = new NBADBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var nba_players = db.NBA_Player.ToList();
+            return View(nba_players);
+        }
+    }
+}
